39c081185fd43c69995901c7036b8a3ffc6ab9d7
Rohan Aaron <rohanaaron16@gmail.com>
2025-02-22 22:57:00 +0530

    Add TinyMCE integration with necessary plugins and configuration

---
+++ resources/views/components/tinymce-editor.blade.php
@@ -0,0 +1,5 @@
+<form method="post">
+    <textarea id="myeditorinstance">Hello, World!</textarea>
+</form>
+
+
---
+++ webpack.mix.cjs
@@ -0,0 +1,3 @@
+const mix = require("laravel-mix");
+
+mix.copyDirectory("vendor/tinymce/tinymce", "public/js/tinymce");
